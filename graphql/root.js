98743ec4a54698ef1831ce597e5c6f9bad1b5011
EG-easy <1018eguchi@gmail.com>
2021-10-03 11:46:55 +0900

    impl fetchValidators

--- graphql/root.js
+++ graphql/root.js
@@ -7,7 +7,8 @@ class Root {
     const resolver = {
       fetchMission: this.service.fetchMission,
       fetchMissions: this.service.fetchMissions,
-      fetchTxCount: this.service.fetchTxCount
+      fetchTxCount: this.service.fetchTxCount,
+      fetchValidators: this.service.fetchValidators
     }
     return resolver
   }
