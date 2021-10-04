7155d734183f45481234bae808e1b1b2a0d107f0
EG-easy <1018eguchi@gmail.com>
2021-10-05 01:44:20 +0900

    fix mission seeder

--- seeders/20210927062824-missions.js
+++ seeders/20210927062824-missions.js
@@ -14,14 +14,14 @@ module.exports = {
      * }], {});
     */
     await queryInterface.bulkInsert('missions', wrap([
-      { point: 10, detail: 'Sign Genesis Block' },
-      { point: 10, detail: 'Share Seed Node' },
-      { point: 10, detail: 'Delegate' },
-      { point: 10, detail: 'Vote' },
-      { point: 10, detail: 'IBC Transfer' },
-      { point: 10, detail: 'Upgrade Node' },
-      { point: 20, detail: 'No Jail' },
-      { point: 20, detail: 'Double Sign Simulation' }
+      { point: 10, name: 'Sign Genesis Block', detail: 'Validator node should be up when starting genesis.' },
+      { point: 10, name: 'Share Seed Node', detail: 'Add your seed node info in this hackmd.' },
+      { point: 10, name: 'Delegate', detail: 'Broadcast delegate tx to at least once.' },
+      { point: 10, name: 'Vote', detail: 'Broadcast vote tx at least 3 times.' },
+      { point: 10, name: 'IBC Transfer', detail: 'Broadcast IBC Transfer tx at least once.' },
+      { point: 10, name: 'Upgrade Node', detail: 'Upgrade Node to the specific version and send new tx.' },
+      { point: 20, name: 'No Jail', detail: 'Never jailed until from genesis to phase4 end.' },
+      { point: 20, name: 'Double Sign Simulation', detail: 'Sign twice in the same hieght block and get jailed intentionally.' }
     ]), {})
   },
 
